d56cedade1475aa88e591180c9a8cff1cd4d73b2
Abel O Okoh <62292802+Abellybaba@users.noreply.github.com>
2022-08-05 05:58:58 -0400

    first commit
    crud app with only insert and display

---
+++ locationWebApi/Controllers/locationRecordController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using locationWebApi.Models;
+
+
+
+namespace locationWebApi.Controllers
+{
+    public class locationRecordController : ApiController
+
+    {
+        newDBEntities nDbObj = new newDBEntities();
+
+        public IHttpActionResult getLocationRec()
+        {
+           
+            IList<locationClass> locObj = nDbObj.locations.Include("locations")
+                .Select(x => new locationClass()
+                {
+                    location_id = x.location_id,
+                    street_address = x.street_address,
+                    postal_code = x.postal_code,
+                    city = x.city,
+                    state_province = x.state_province,
+                    country_id = x.country_id
+
+                }).ToList<locationClass>();
+
+            return Ok(locObj);
+        }
+
+        public IHttpActionResult insertLocationRec(location loc)
+        {
+            nDbObj.locations.Add(loc);
+            nDbObj.SaveChanges();
+            return Ok("The Record was inserted Successfully!");
+        }
+
+    }
+}
---
+++ locationWebApi/Models/locationClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace locationWebApi.Models
+{
+    public class locationClass
+    {
+        public int location_id { get; set; }
+        public string street_address { get; set; }
+        public string postal_code { get; set; }
+        public string city { get; set; }
+        public string state_province { get; set; }
+        public string country_id { get; set; }
+
+    }
+}
